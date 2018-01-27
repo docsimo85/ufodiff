1e6f94ed55d2ac17c00da82ab5f5fcfbbdff9edc
DESKTOP-3ATA7IU\Simo <docsimo85@gmail.com>
2018-01-27 17:14:33 +0100

    no message

--- execute.php
+++ execute.php
@@ -1,11 +1,12 @@
 <?php
-$content = file_get_contents("php://input");
+$content = file_get_contents("php://input"); 
 $update = json_decode($content, true);
+ 
 if(!$update){
   exit;
 }
+ 
 $message = isset($update['message']) ? $update['message'] : "";
-$json = file_get_contents('https://chainz.cryptoid.info/explorer/api.dws?q=summary');
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
@@ -13,10 +14,15 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
+ 
 $text = trim($text);
 $text = strtolower($text);
+ 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => 'testo');
+$json = file_get_contents("https://chainz.cryptoid.info/explorer/api.dws?q=summary");
+$parameters = array('chat_id' => $chatId, "text" => json_encode($json));
 $parameters["method"] = "sendMessage";
-$parameters["reply_markup"] = '{ "keyboard": [["uno", "due"], ["tre", "quattro"], ["cinque"]], "resize_keyboard": true, "one_time_keyboard": false}';
+ 
+//$parameters["reply_markup"] = '{ "keyboard": [["uno", "due"], ["tre", "quattro"], ["cinque"]], "resize_keyboard": true, "one_time_keyboard": false}';
+ 
 echo json_encode($parameters);
