918557371e38ca1f5d3aa034673ce42a36f9e037
DESKTOP-3ATA7IU\Simo <docsimo85@gmail.com>
2018-01-28 12:36:53 +0100

    no message

--- execute.php
+++ execute.php
@@ -5,7 +5,7 @@ if(!$update){
   exit;
 }
 $message = isset($update['message']) ? $update['message'] : "";
-$json = file_get_contents('http://echo.jsontest.com/key/value/one/two');
+$json = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=getdifficulty');
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
