f7cdfd381e7d7430c42fc6b694cad7f1c95c9341
DESKTOP-3ATA7IU\Simo <docsimo85@gmail.com>
2018-01-28 13:12:32 +0100

    no message

--- execute.php
+++ execute.php
@@ -6,6 +6,7 @@ if(!$update){
 }
 $message = isset($update['message']) ? $update['message'] : "";
 $json = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=getdifficulty');
+$json2 = file_get_contents('https://chainz.cryptoid.info/ufo/api.dws?q=getblockcount');
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
 $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
@@ -16,7 +17,7 @@ $text = isset($message['text']) ? $message['text'] : "";
 $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => json_decode($json,true));
+$parameters = array('chat_id' => $chatId, "text" => json_decode($json,true).json_decode($json2,true));
 $parameters["method"] = "sendMessage";
 $parameters["reply_markup"] = '{ "keyboard": [["uno", "due"], ["tre", "quattro"], ["cinque"]], "resize_keyboard": true, "one_time_keyboard": false}';
 echo json_encode($parameters);
