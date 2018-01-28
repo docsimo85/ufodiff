5b51c24d49690ca8c0000de94bae03199dc179c9
doc <consulente.gaido@serijakala.com>
2018-01-28 18:47:28 +0100

    no message

--- execute.php
+++ execute.php
@@ -22,9 +22,15 @@ $parameters = array('chat_id' => $chatId, "text" => 'UFO coin difficulty and blo
 $parameters["method"] = "sendMessage";
 //$parameters["reply_markup"] = '{ "keyboard": [["uno", "due"], ["tre", "quattro"], ["cinque"]], "resize_keyboard": true, "one_time_keyboard": false}';
 }
-else if($text == '!help'){
+else if($text == '!help' || $text=='/help'){
 header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => 'UFO coin difficulty and block height Bot'.chr(10).'Just type !network and bot will reply with difficulty and block height.'.chr(10).'If you find it useful donations are welcome :) UFO Address: BwJvr6HVnnsHRK7PArc72yrLXYEe52yAYp'); 
 $parameters["method"] = "sendMessage";
-};
+}
+else if($text == '/start'){
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => 'UFO coin difficulty and block height Bot'.chr(10).'Just type !network and bot will reply with difficulty and block height.'.chr(10).'If you find it useful donations are welcome :) UFO Address: BwJvr6HVnnsHRK7PArc72yrLXYEe52yAYp'); 
+$parameters["method"] = "sendMessage";
+}
+;
 echo json_encode($parameters);
