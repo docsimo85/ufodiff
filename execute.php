90e9c40983c4486002ba7665d86ec2819680f1c9
doc <consulente.gaido@serijakala.com>
2018-01-28 18:35:54 +0100

    no message

--- execute.php
+++ execute.php
@@ -21,5 +21,15 @@ header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "text" => 'UFO coin difficulty and block height:'.chr(10).'Current diff: '.json_decode($json,true).chr(10).'Current block: '.json_decode($json2,true)); 
 $parameters["method"] = "sendMessage";
 //$parameters["reply_markup"] = '{ "keyboard": [["uno", "due"], ["tre", "quattro"], ["cinque"]], "resize_keyboard": true, "one_time_keyboard": false}';
+}
+else if($text = '!help'){
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => 'UFO coin difficulty and block height Bot'.chr(10).'Just type !network and bot will reply with difficulty and block height.'.chr(10).'If you find it useful donations are welcome :) UFO Address: BwJvr6HVnnsHRK7PArc72yrLXYEe52yAYp'); 
+$parameters["method"] = "sendMessage";
+};
+else if($text = '/start'){
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => 'UFO coin difficulty and block height Bot'.chr(10).'Just type !network and bot will reply with difficulty and block height.'.chr(10).'If you find it useful donations are welcome :) UFO Address: BwJvr6HVnnsHRK7PArc72yrLXYEe52yAYp'); 
+$parameters["method"] = "sendMessage";
 };
 echo json_encode($parameters);
