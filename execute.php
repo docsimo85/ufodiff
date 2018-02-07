01e74e73df1d219d4e19b08adbc173051af09ae8
SSYSTEM\sgaido <consulente.gaido@serijakala.com>
2018-02-07 11:31:33 +0100

    x

--- execute.php
+++ execute.php
@@ -26,7 +26,7 @@ $parameters = array('chat_id' => $chatId, "text" =>
         'qui stampo la lista'
 );
 $parameters["method"] = "sendMessage";
-$parameters["reply_markup"] = '{ "keyboard": ["Aggiungi altro prodotto", "Termina inserimento"], "resize_keyboard": true, "one_time_keyboard": true}';
+//$parameters["reply_markup"] = '{ "keyboard": ["Aggiungi altro prodotto", "Termina inserimento"], "resize_keyboard": true, "one_time_keyboard": false}';
 }
 else if($text == 'aiuto'){
 header("Content-Type: application/json");
@@ -34,13 +34,13 @@ $parameters = array('chat_id' => $chatId, "text" =>
         'lista comandi');
 $parameters["method"] = "sendMessage";
 }
-else if($text == 'aggiungi'){
+else if(strpos($text, 'aggiungi')){
     header("Content-Type: application/json");
     $parameters = array('chat_id' => $chatId, "text" =>
         'comando per aggiungere prodotti');
     $parameters["method"] = "sendMessage";
 }
-else if($text == 'rimuovi'){
+else if(strpos($text, 'rimuovi')){
     header("Content-Type: application/json");
     $parameters = array('chat_id' => $chatId, "text" =>
         'comando per rimuovere prodotti');
